95f919995cd2e14cdd8c10aae6504b2b88db418f
Che4ter <che4ter@42.wtf>
2017-11-10 08:56:32 +0100

    fix heroku deploy

--- src/main/java/com/github/stairch/RestInPeace.java
+++ src/main/java/com/github/stairch/RestInPeace.java
@@ -47,9 +47,7 @@ public class RestInPeace {
         final HttpServer server = startServer();
         StaticHttpHandler staticHttpHandler = new StaticHttpHandler("src/main/java/com/github/stairch/static");
         server.getServerConfiguration().addHttpHandler(staticHttpHandler, "/static");
-        System.out.println("Your java starter snake is available at " + BASE_URI + "\nHit enter to stop it...");
-        System.in.read();
-        server.stop();
+        System.out.println("Your java starter snake is available at " + BASE_URI + "\nHit ctrl+c to stop it...");
     }
 }
 
