4145d39a581e85b23f185a0e9466ec6985bc4eff
tdsummer <oliver.summermatter@stud.hslu.ch>
2017-11-11 14:01:32 +0100

    killthemall

--- src/main/java/com/github/stairch/rest/SnakeService.java
+++ src/main/java/com/github/stairch/rest/SnakeService.java
@@ -58,34 +58,45 @@ public class SnakeService {
         foodList.get(0).getX();
         foodList.get(0).getY();
 
-        List<SnakeDTO> snake = moveRequestDTO.getSnakes();
+
+        List<SnakeDTO> snakeList = moveRequestDTO.getSnakes();
 
         PointDTO head = new PointDTO();
-        for (int i = 0; i < snake.size(); i++) {
-            if (snake.get(i).getId() == moveRequestDTO.getYou()) {
-                int tmp = snake.get(i).getCoordsAsPoints().get(0).getX();
+        for (int i = 0; i < snakeList.size(); i++) {
+            System.out.println("ID-i:"+snakeList.get(i).getId());
+            System.out.println("My-ID-i:"+moveRequestDTO.getYou());
+            if (snakeList.get(i).getId().equals(moveRequestDTO.getYou())) {
+                int tmp = snakeList.get(i).getCoordsAsPoints().get(0).getX();
                 head.setX(tmp);
-                tmp = snake.get(i).getCoordsAsPoints().get(0).getY();
+                tmp = snakeList.get(i).getCoordsAsPoints().get(0).getY();
                 head.setY(tmp);
             }
         }
+
+        System.out.println("FoodX: " + foodList.get(0).getX());
+        System.out.println("FoodY: " + foodList.get(0).getY());
+        System.out.println("HeadX: " + head.getX());
+        System.out.println("HeadY: " + head.getY());
+
 
         //System.out.println("ToString" + moveRequest.toString());
 
         final MoveResponseDTO moveResponse = new MoveResponseDTO();
         //moveResponse.setMove(Move.left);
 
-        if (head.getX() > foodList.get(0).getX()) {
-            moveResponse.setMove(Move.left);
-        } else {
-            moveResponse.setMove(Move.right);
+        if (head.getY() == foodList.get(0).getY()) {
+            if (head.getX() > foodList.get(0).getX()) {
+                moveResponse.setMove(Move.left);
+            } else {
+                moveResponse.setMove(Move.right);
+            }
+        }else{
+            if (head.getY() > foodList.get(0).getY()) {
+                moveResponse.setMove(Move.up);
+            } else {
+                moveResponse.setMove(Move.down);
+            }
         }
-        if (head.getY() > foodList.get(0).getY()) {
-            moveResponse.setMove(Move.down);
-        } else {
-            moveResponse.setMove(Move.up);
-        }
-
 
         // System.out.println(moveResponse.toString());
 
